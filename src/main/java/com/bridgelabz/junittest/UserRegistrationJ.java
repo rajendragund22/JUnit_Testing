bb16f856c38b18702fdfdecef990fbcf29f23b50
RajendraGund <rajendragund2015@gmail.com>
2021-09-16 07:49:28 +0530

    Added : Welcome message for the User in Junit Testing program

---
+++ src/main/java/com/bridgelabz/junittest/UserRegistrationJ.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.junittest;
+
+public class UserRegistrationJ {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Junit Testing Program !!! ");
+    }
+}
+
